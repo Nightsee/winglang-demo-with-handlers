360b9b19d93bd888d7075c80816612a05b0dc4d4
Nightsee <younes.naitsi.2002e@gmail.com>
2024-07-29 09:47:32 +0100

    rm *.ts

--- Handlers/Add.extern.d.ts
+++
@@ -1,3 +0,0 @@
-export default interface extern {
-  Add: (count: number, addToDb: (arg0: Readonly<any>) => Promise<void>, data: Readonly<any>) => Promise<string>,
-}
--- Handlers/Delete.extern.d.ts
+++
@@ -1,3 +0,0 @@
-export default interface extern {
-  Delete: (deleteFromDb: (arg0: Readonly<any>) => Promise<void>, taskId: Readonly<any>) => Promise<string>,
-}
